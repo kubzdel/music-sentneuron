9c99bb5698a3d6b00b4346d015546d3782393b42
KWencierski <kajetan.wencierski@student.put.poznan.pl>
2022-08-08 19:19:50 +0200

    Add typical sampling and grid search for optimal sampling hyperparameters

--- midi_generator.py
+++ midi_generator.py
@@ -2,99 +2,147 @@ import os
 import json
 import argparse
 import numpy as np
-import tensorflow as tf
-import midi_encoder as me
+# import midi_encoder as me
+# from train_generative import build_generative_model
+# from train_classifier import preprocess_sentence
 
-from train_generative import build_generative_model
-from train_classifier import preprocess_sentence
+from tokenizer import MidiTokenizer
+from transformer_generative import MidiTrainingModule
+from pytorch_lightning.loggers import MLFlowLogger
+from pytorch_lightning import Trainer
+from dotenv import load_dotenv
 
 GENERATED_DIR = './generated'
 
-def override_neurons(model, layer_idx, override):
-    h_state, c_state = model.get_layer(index=layer_idx).states
 
-    c_state = c_state.numpy()
-    for neuron, value in override.items():
-        c_state[:,int(neuron)] = int(value)
+# def override_neurons(model, layer_idx, override):
+#     h_state, c_state = model.get_layer(index=layer_idx).states
+#
+#     c_state = c_state.numpy()
+#     for neuron, value in override.items():
+#         c_state[:,int(neuron)] = int(value)
+#
+#     model.get_layer(index=layer_idx).states = (h_state, tf.Variable(c_state))
+#
+#
+# def sample_next(predictions, k):
+#     # Sample using a categorical distribution over the top k midi chars
+#     top_k = tf.math.top_k(predictions, k)
+#     top_k_choices = top_k[1].numpy().squeeze()
+#     top_k_values = top_k[0].numpy().squeeze()
+#
+#     if np.random.uniform(0, 1) < .5:
+#         predicted_id = top_k_choices[0]
+#     else:
+#         p_choices = tf.math.softmax(top_k_values[1:]).numpy()
+#         predicted_id = np.random.choice(top_k_choices[1:], 1, p=p_choices)[0]
+#
+#     return predicted_id
+#
+#
+# def process_init_text(model, init_text, char2idx, layer_idx, override):
+#     model.reset_states()
+#
+#     for c in init_text.split(" "):
+#         # Run a forward pass
+#         try:
+#             input_eval = tf.expand_dims([char2idx[c]], 0)
+#
+#             # override sentiment neurons
+#             override_neurons(model, layer_idx, override)
+#
+#             predictions = model(input_eval)
+#         except KeyError:
+#             if c != "":
+#                 print("Can't process char", s)
+#
+#     return predictions
+#
+#
+# def generate_midi(model, char2idx, idx2char, init_text="", seq_len=256, k=3, layer_idx=-2, override={}):
+#     # Add front and end pad to the initial text
+#     init_text = preprocess_sentence(init_text)
+#
+#     # Empty midi to store our results
+#     midi_generated = []
+#
+#     # Process initial text
+#     predictions = process_init_text(model, init_text, char2idx, layer_idx, override)
+#
+#     # Here batch size == 1
+#     model.reset_states()
+#     for i in range(seq_len):
+#         # remove the batch dimension
+#         predictions = tf.squeeze(predictions, 0).numpy()
+#
+#         # Sample using a categorical distribution over the top k midi chars
+#         predicted_id = sample_next(predictions, k)
+#
+#          # Append it to generated midi
+#         midi_generated.append(idx2char[predicted_id])
+#
+#         # override sentiment neurons
+#         override_neurons(model, layer_idx, override)
+#
+#         #Run a new forward pass
+#         input_eval = tf.expand_dims([predicted_id], 0)
+#         predictions = model(input_eval)
+#
+#     return init_text + " " + " ".join(midi_generated)
 
-    model.get_layer(index=layer_idx).states = (h_state, tf.Variable(c_state))
+def get_latest_checkpoint_file(checkpoint_dir):
+    checkpoints = [os.path.join(checkpoint_dir, f) for f in os.listdir(checkpoint_dir) if f.endswith('.ckpt')]
+    latest_checkpoint = max(checkpoints, key=os.path.getctime)
+    return latest_checkpoint
 
-def sample_next(predictions, k):
-    # Sample using a categorical distribution over the top k midi chars
-    top_k = tf.math.top_k(predictions, k)
-    top_k_choices = top_k[1].numpy().squeeze()
-    top_k_values = top_k[0].numpy().squeeze()
 
-    if np.random.uniform(0, 1) < .5:
-        predicted_id = top_k_choices[0]
-    else:
-        p_choices = tf.math.softmax(top_k_values[1:]).numpy()
-        predicted_id = np.random.choice(top_k_choices[1:], 1, p=p_choices)[0]
+def grid_search(top_ks, top_ps, temperatures, typical_tau):
+    for top_k in top_ks:
+        for top_p in top_ps:
+            for temperature in temperatures:
+                for tau in typical_tau:
+                    yield top_k, top_p, temperature, tau
 
-    return predicted_id
 
-def process_init_text(model, init_text, char2idx, layer_idx, override):
-    model.reset_states()
+def sampling_testing_grid_search(top_ks, top_ps, temperatures, typical_taus):
+    """
+    Generate midi file for each combination of parameters of the model.
+    :param top_ks: list of top_k values to test
+    :param top_ps: list of top_p values to test
+    :param temperatures: list of temperatures to test
+    :param typical_taus: list of typical taus to test
+    """
+    tokenizer = MidiTokenizer(from_file='trained/char2idx.json')
+    checkpoint = get_latest_checkpoint_file(opt.model)
+    load_dotenv()
 
-    for c in init_text.split(" "):
-        # Run a forward pass
-        try:
-            input_eval = tf.expand_dims([char2idx[c]], 0)
+    for top_k, top_p, temperature, tau in grid_search(top_ks, top_ps, temperatures, typical_taus):
+        model = MidiTrainingModule.load_from_checkpoint(checkpoint, tokenizer=tokenizer,
+                                                        top_k=top_k, top_p=top_p, temperature=temperature,
+                                                        typical_tau=tau)
+        model.seq_len = opt.seqlen
+        mlf_logger = MLFlowLogger(experiment_name="sampling-midi", tracking_uri="http://150.254.131.193:5000/",
+                                  run_name=opt.run_name)
+        trainer = Trainer(logger=mlf_logger)
+        trainer.predict(model=model)
 
-            # override sentiment neurons
-            override_neurons(model, layer_idx, override)
-
-            predictions = model(input_eval)
-        except KeyError:
-            if c != "":
-                print("Can't process char", s)
-
-    return predictions
-
-def generate_midi(model, char2idx, idx2char, init_text="", seq_len=256, k=3, layer_idx=-2, override={}):
-    # Add front and end pad to the initial text
-    init_text = preprocess_sentence(init_text)
-
-    # Empty midi to store our results
-    midi_generated = []
-
-    # Process initial text
-    predictions = process_init_text(model, init_text, char2idx, layer_idx, override)
-
-    # Here batch size == 1
-    model.reset_states()
-    for i in range(seq_len):
-        # remove the batch dimension
-        predictions = tf.squeeze(predictions, 0).numpy()
-
-        # Sample using a categorical distribution over the top k midi chars
-        predicted_id = sample_next(predictions, k)
-
-         # Append it to generated midi
-        midi_generated.append(idx2char[predicted_id])
-
-        # override sentiment neurons
-        override_neurons(model, layer_idx, override)
-
-        #Run a new forward pass
-        input_eval = tf.expand_dims([predicted_id], 0)
-        predictions = model(input_eval)
-
-    return init_text + " " + " ".join(midi_generated)
 
 if __name__ == "__main__":
-
     # Parse arguments
     parser = argparse.ArgumentParser(description='midi_generator.py')
+    parser.add_argument('--run_name', type=str, required=True, help='MLFlow run name')
     parser.add_argument('--model', type=str, required=True, help="Checkpoint dir.")
     parser.add_argument('--ch2ix', type=str, required=True, help="JSON file with char2idx encoding.")
     parser.add_argument('--embed', type=int, required=True, help="Embedding size.")
-    parser.add_argument('--units', type=int, required=True, help="LSTM units.")
-    parser.add_argument('--layers', type=int, required=True, help="LSTM layers.")
+    parser.add_argument('--model_type', type=str, default='transformer', help="Transformer or LSTM.")
+    parser.add_argument('--n_head', type=int, default=512, help="Transformer heads.")
+    parser.add_argument('--units', type=int, default=512, help="LSTM units.")
+    parser.add_argument('--layers', type=int, default=4, help="LSTM layers.")
     parser.add_argument('--seqinit', type=str, default="\n", help="Sequence init.")
-    parser.add_argument('--seqlen', type=int, default=256, help="Sequence lenght.")
+    parser.add_argument('--seqlen', type=int, default=256, help="Sequence length.")
     parser.add_argument('--cellix', type=int, default=-2, help="LSTM layer to use as encoder.")
     parser.add_argument('--override', type=str, default="", help="JSON file with neuron values to override.")
+    parser.add_argument('--grid_search', type=bool, default=False, help="Run a grid search.")
     opt = parser.parse_args()
 
     # Load char2idx dict from json file
@@ -111,18 +159,42 @@ if __name__ == "__main__":
         print("Override JSON file not provided.")
 
     # Create idx2char from char2idx dict
-    idx2char = {idx:char for char,idx in char2idx.items()}
+    idx2char = {idx: char for char, idx in char2idx.items()}
 
     # Calculate vocab_size from char2idx dict
     vocab_size = len(char2idx)
 
     # Rebuild model from checkpoint
-    model = build_generative_model(vocab_size, opt.embed, opt.units, opt.layers, batch_size=1)
-    model.load_weights(tf.train.latest_checkpoint(opt.model))
-    model.build(tf.TensorShape([1, None]))
+    if opt.model_type.lower() == 'transformer':
+        if opt.grid_search:
+            sampling_testing_grid_search(top_ks=[0], top_ps=[1.0], temperatures=[1.4, 1.5],
+                                         typical_taus=[0.2, 0.25, 0.3, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+        else:
+            tokenizer = MidiTokenizer(from_file='trained/char2idx.json')
+            checkpoint = get_latest_checkpoint_file(opt.model)
+            model = MidiTrainingModule.load_from_checkpoint(checkpoint, tokenizer=tokenizer,
+                                                            top_k=0, top_p=1, temperature=1.5, typical_tau=0.7)
+            model.seq_len = opt.seqlen
+            mlf_logger = MLFlowLogger(experiment_name="sampling-midi", tracking_uri="http://150.254.131.193:5000/",
+                                      run_name=opt.run_name)
+            load_dotenv()
+            trainer = Trainer(logger=mlf_logger)
+            midi_txt = trainer.predict(model=model, dataloaders=None)
+            print(midi_txt)
+
+    elif opt.model_type.lower() == 'lstm':
+        # TODO - implement pl LSTM support
+        # model = build_generative_model(vocab_size, opt.embed, opt.units, opt.layers, batch_size=1)
+        # model.load_weights(tf.train.latest_checkpoint(opt.model))
+        # model.build(tf.TensorShape([1, None]))
+        midi_txt = 'LSTM not implemented yet'
+
+    else:
+        print("Invalid model type.")
+        exit(1)
 
     # Generate a midi as text
-    midi_txt = generate_midi(model, char2idx, idx2char, opt.seqinit, opt.seqlen, layer_idx=opt.cellix, override=override)
-    print(midi_txt)
+    # midi_txt = generate_midi(model, char2idx, idx2char, opt.seqinit, opt.seqlen, layer_idx=opt.cellix, override=override)
+    # print(midi_txt)
 
-    me.write(midi_txt, os.path.join(GENERATED_DIR, "generated.mid"))
+    # me.write(midi_txt, os.path.join(GENERATED_DIR, "generated.mid"))
--- transformer_generative.py
+++ transformer_generative.py
@@ -6,10 +6,15 @@ import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
 from torch.optim import AdamW
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
 
 from midi_encoder import write
 from models import TransformerModel
 from utils import top_k_top_p_filtering
+from transformers.generation_logits_process import TypicalLogitsWarper
+
+SLIDING_WINDOW_SIZE = 256
 
 
 class MidiTrainingModule(pl.LightningModule):
@@ -19,7 +24,8 @@ class MidiTrainingModule(pl.LightningModule):
     """
 
     def __init__(self, n_layer, n_head, n_embd, seq_len,
-                 batch_size, epochs, samples_count, tokenizer, vocab_size, training_stride, validation_stride) -> None:
+                 batch_size, epochs, samples_count, tokenizer, vocab_size, training_stride, validation_stride,
+                 top_k=0, top_p=1, temperature=1.5, typical_tau=0.7) -> None:
         super(MidiTrainingModule, self).__init__()
         self.batch_size = batch_size
         self.epochs = epochs
@@ -33,42 +39,58 @@ class MidiTrainingModule(pl.LightningModule):
         self.loss = torch.nn.CrossEntropyLoss()
         self.seq_len = seq_len
         self.save_hyperparameters()
+        self.top_k = top_k
+        self.top_p = top_p
+        self.temperature = temperature
+        self.typical_tau = typical_tau
 
-    def generate(self, step) -> dict:
+    def generate(self) -> dict:
         """ Predict function.
-        :param sample: dictionary with the text we want to classify.
         Returns:
-            Dictionary with the input text and the predicted label.
+            Generated music midi sentence.
         """
 
         self.model.eval()
         self.model.to('cpu')
         predicted_token = torch.Tensor([1])
         with torch.no_grad():
-            output_seq = self.tokenizer.encode('\n', return_tensors=True)
+            sliding_window = self.tokenizer.encode('\n', return_tensors=True)
+            output_seq = torch.Tensor([])
             while (
                     len(output_seq) < self.seq_len - 1 and predicted_token.unsqueeze(-1)[0] != 0
             ):
-                outputs = self.forward(output_seq, permute=False)
+                outputs = self.forward(sliding_window, permute=False)
                 lm_logits = outputs
                 logits = lm_logits[-1, :]
-                top_k, top_p, temperature = 0, 0.95, 1
-                filtered_logits = top_k_top_p_filtering(
-                    logits, top_k=top_k, top_p=top_p, temperature=temperature
-                )
+
+                if int(self.typical_tau) != 1:
+                    typical_logits_wraper = TypicalLogitsWarper(mass=self.typical_tau)
+                    lm_logits = lm_logits / self.temperature
+                    filtered_logits = typical_logits_wraper(torch.LongTensor(), lm_logits)[-1, :]
+                else:
+                    filtered_logits = top_k_top_p_filtering(
+                        logits, top_k=self.top_k, top_p=self.top_p, temperature=self.temperature
+                    )
+
+                # # Ferreira
+                # if np.random.uniform(0, 1) < .5:
+                #     filtered_logits = top_k_top_p_filtering(logits, top_k=1, top_p=1, temperature=1)
+                # else:
+                #     index_to_remove = logits == torch.topk(logits, 1)[0][..., -1, None]
+                #     logits[index_to_remove] = -float("Inf")
+                #     filtered_logits = top_k_top_p_filtering(logits, top_k=self.top_k, top_p=1, temperature=1)
+
                 probabilities = F.softmax(filtered_logits, dim=-1)
-                probabilities_logits, probabilities_position = torch.sort(
-                    probabilities, descending=True
-                )
                 predicted_token = torch.multinomial(probabilities, 1)
+                if len(sliding_window) < SLIDING_WINDOW_SIZE:
+                    sliding_window = torch.cat([sliding_window, predicted_token])
+                else:
+                    sliding_window = torch.cat([sliding_window[1:], predicted_token])
                 output_seq = torch.cat([output_seq, predicted_token])
-            output_seq = output_seq[1:-1]
+            output_seq = output_seq[:-1]
+            print(f'\nSequence length: {len(output_seq)}')
             output_sentence = self.tokenizer.decode(output_seq)
             print(output_sentence)
-            Path(f'{self.run_name}_generated_samples').mkdir(exist_ok=True)
-            midi_path = f'{self.run_name}_generated_samples/{self.run_name}_step_{str(step)}.mid'
-            write(output_sentence, midi_path)
-            self.logger.experiment.log_artifact(run_id=self.logger.run_id, local_path=midi_path)
         self.model.cuda()
         self.model.train()
         return output_sentence
@@ -114,7 +136,12 @@ class MidiTrainingModule(pl.LightningModule):
             # print(self.metrics_manager.valid_results_to_string())
             self.log('val_loss', avg_loss, on_epoch=True, prog_bar=True)
             self.logger.log_metrics({'val_loss': avg_loss}, step=self.global_step)
-        self.generate(self.global_step)
+        generated_sentence = self.generate()
+        Path(f'{self.run_name}_generated_samples').mkdir(exist_ok=True)
+        midi_path = f'{self.run_name}_generated_samples/{self.run_name}_step_{str(self.global_step)}.mid'
+        write(generated_sentence, midi_path)
+        self.logger.experiment.log_artifact(run_id=self.logger.run_id, local_path=midi_path)
+
 
     def configure_optimizers(self):
         """ Sets Learning rate for different parameter groups. """
@@ -129,3 +156,17 @@ class MidiTrainingModule(pl.LightningModule):
         self.sched = torch.optim.lr_scheduler.CosineAnnealingLR(
             optimizer, T_max=t_total)
         return [optimizer], [{"scheduler": self.sched, "interval": "step"}]
+
+    def predict_step(self, batch, batch_idx: int, dataloader_idx: int = 0):
+        self.run_name = self.logger._run_name
+        output_sentence = self.generate()
+        Path(f'{self.run_name}_generated_samples').mkdir(exist_ok=True)
+        midi_path = f'{self.run_name}_generated_samples/{self.run_name}' \
+                    f'_k{str(self.top_k)}_p{str(self.top_p)}_temp{str(self.temperature)}' \
+                    f'_tau{str(self.typical_tau)}.mid'
+        write(output_sentence, midi_path)
+        self.logger.experiment.log_artifact(run_id=self.logger.run_id, local_path=midi_path)
+        return output_sentence
+
+    def predict_dataloader(self):
+        return DataLoader([1], 1)
